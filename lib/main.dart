81195470e9f42919377adde98f8bc7dd524bf70b
nodachizi <billrock2012@hotmail.com>
2021-04-21 20:07:15 +0700

    finalflutter

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lit_firebase_auth/lit_firebase_auth.dart';
+import 'package:fluttermk2/config/palette.dart';
+import 'package:fluttermk2/screens/splash.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'screens/home.dart';
+import 'screens/auth/auth.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LitAuthInit(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        theme: ThemeData(
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          textTheme: GoogleFonts.muliTextTheme(),
+          accentColor: Palette.darkOrange,
+          appBarTheme: const AppBarTheme(
+            brightness: Brightness.dark,
+            color: Palette.darkBlue,
+          ),
+        ),
+
+        // home: const LitAuthState(
+        //   authenticated: Home(),
+        //   unauthenticated: Unauthenticated(),
+        // ),
+        home: const SplashScreen(),
+      ),
+    );
+  }
+}
